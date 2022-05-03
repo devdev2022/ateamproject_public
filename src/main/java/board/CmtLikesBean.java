db7d9f6d37974e4b127ad95218ab1b679f466f3e
KwakDupal <104192651+KwakDupal@users.noreply.github.com>
2022-05-03 11:17:09 +0900

    Create CmtLikesBean.java

---
+++ src/main/java/board/CmtLikesBean.java
@@ -0,0 +1,29 @@
+package board;
+
+public class CmtLikesBean {
+
+	private int cnum;
+	private int num;
+	private String id;
+	
+	public int getCnum() {
+		return cnum;
+	}
+	public void setCnum(int cnum) {
+		this.cnum = cnum;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
+}
