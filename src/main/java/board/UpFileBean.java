b244c7b178f922b3bcd149410af2dec1f1ccdb46
KwakDupal <104192651+KwakDupal@users.noreply.github.com>
2022-05-03 13:43:01 +0900

    Create UpFileBean.java

---
+++ src/main/java/board/UpFileBean.java
@@ -0,0 +1,30 @@
+package board;
+
+public class UpFileBean {
+	
+	private int num;
+	private String filename;
+	private int filesize;
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getFilename() {
+		return filename;
+	}
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+	public int getFilesize() {
+		return filesize;
+	}
+	public void setFilesize(int filesize) {
+		this.filesize = filesize;
+	}
+	
+	
+	
+}
