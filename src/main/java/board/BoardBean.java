4691c37bb7451aebc6f19135a260920c609d8a98
KwakDupal <104192651+KwakDupal@users.noreply.github.com>
2022-05-03 11:08:21 +0900

    Create BoardBean.java

---
+++ src/main/java/board/BoardBean.java
@@ -0,0 +1,121 @@
+package board;
+
+public class BoardBean {
+		
+	private int num;
+	private String id;
+	private String subject;
+	private String content;
+	private int pos;
+	private int ref;
+	private int depth;
+	private String refdate;
+	private String ip;
+	private int count;
+	private int type_board;
+	private int type_cat;
+	private String fmus_name;
+	private int fmus_size;
+	private String fimg_name;
+	private int fimg_int;
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getPos() {
+		return pos;
+	}
+	public void setPos(int pos) {
+		this.pos = pos;
+	}
+	public int getRef() {
+		return ref;
+	}
+	public void setRef(int ref) {
+		this.ref = ref;
+	}
+	public int getDepth() {
+		return depth;
+	}
+	public void setDepth(int depth) {
+		this.depth = depth;
+	}
+	public String getRefdate() {
+		return refdate;
+	}
+	public void setRefdate(String refdate) {
+		this.refdate = refdate;
+	}
+	public String getIp() {
+		return ip;
+	}
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	public int getType_board() {
+		return type_board;
+	}
+	public void setType_board(int type_board) {
+		this.type_board = type_board;
+	}
+	public int getType_cat() {
+		return type_cat;
+	}
+	public void setType_cat(int type_cat) {
+		this.type_cat = type_cat;
+	}
+	public String getFmus_name() {
+		return fmus_name;
+	}
+	public void setFmus_name(String fmus_name) {
+		this.fmus_name = fmus_name;
+	}
+	public int getFmus_size() {
+		return fmus_size;
+	}
+	public void setFmus_size(int fmus_size) {
+		this.fmus_size = fmus_size;
+	}
+	public String getFimg_name() {
+		return fimg_name;
+	}
+	public void setFimg_name(String fimg_name) {
+		this.fimg_name = fimg_name;
+	}
+	public int getFimg_int() {
+		return fimg_int;
+	}
+	public void setFimg_int(int fimg_int) {
+		this.fimg_int = fimg_int;
+	}
+	
+	
+	
+}
