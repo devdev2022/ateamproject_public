df22ccd6a524965e5bd9e164e247096c6283e68e
KwakDupal <104192651+KwakDupal@users.noreply.github.com>
2022-05-04 09:16:42 +0900

    Update table.sql

--- src/main/webapp/table.sql
+++ src/main/webapp/table.sql
@@ -12,8 +12,8 @@ CREATE TABLE tblboard (
  	ip VARCHAR(15)  NULL,								/*ip주소*/
  	count smallint unsigned  NULL,					/*조회수*/
 
-  	type_board smallint UNSIGNED  ,					/*게시판 분류 코드*/
- 	type_cat smallint UNSIGNED  ,  					/*게시글 카테고리 분류 코드*/
+  	type_board varchar(5)   ,					/*게시판 분류 ex)자유, 공지, 등... */
+ 	type_cat varchar(5)  ,  					/*게시글 카테고리 분류 ex)힙합, 발라드, 등...*/
   PRIMARY KEY (`num`)
 )
 
