a1fa12c13f8ad9520804dcf4773ef4340d37f47e
KwakDupal <104192651+KwakDupal@users.noreply.github.com>
2022-05-13 16:30:50 +0900

    .

---
+++ src/main/java/chart/ChartBean.java
@@ -0,0 +1,79 @@
+package chart;
+
+public class ChartBean {
+	
+	private int chnum;
+	private int ranking;
+	private String artist;
+	private String title;
+	private String albumImg;
+	private String albumName;
+	private String getDate;
+	private String menuId;
+	private String songId;
+	private String lyric;
+	
+	public int getChnum() {
+		return chnum;
+	}
+	public void setChnum(int chnum) {
+		this.chnum = chnum;
+	}
+	public int getRanking() {
+		return ranking;
+	}
+	public void setRanking(int ranking) {
+		this.ranking = ranking;
+	}
+	public String getArtist() {
+		return artist;
+	}
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAlbumImg() {
+		return albumImg;
+	}
+	public void setAlbumImg(String albumImg) {
+		this.albumImg = albumImg;
+	}
+	public String getAlbumName() {
+		return albumName;
+	}
+	public void setAlbumName(String albumName) {
+		this.albumName = albumName;
+	}
+	public String getGetDate() {
+		return getDate;
+	}
+	public void setGetDate(String getDate) {
+		this.getDate = getDate;
+	}
+	public String getMenuId() {
+		return menuId;
+	}
+	public void setMenuId(String menuId) {
+		this.menuId = menuId;
+	}
+	public String getLyric() {
+		return lyric;
+	}
+	public void setLyric(String lyric) {
+		this.lyric = lyric;
+	}
+	public String getSongId() {
+		return songId;
+	}
+	public void setSongId(String songId) {
+		this.songId = songId;
+	}
+	
+	
+	
+}
